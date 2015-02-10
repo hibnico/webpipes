d7d29276b1a06f9f1442cc43a74eccb9930adcfd
Nicolas Lalevée <nicolas.lalevee@hibnet.org>
2015-02-11 00:15:57 +0100

    add uglify2, wip

---
+++ webpipes-uglify2/src/java/org/hibnet/webpipes/processor/uglify/UglifyJsProcessor.java
@@ -0,0 +1,69 @@
+/*
+ *  Copyright 2014-2015 WebPipes contributors
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hibnet.webpipes.processor.uglify;
+
+import org.hibnet.webpipes.Webpipe;
+import org.hibnet.webpipes.WebpipeOutput;
+import org.hibnet.webpipes.processor.ProcessingWebpipe;
+import org.hibnet.webpipes.processor.ProcessingWebpipeFactory;
+
+/**
+ * Compress js using uglifyJs utility.
+ */
+public class UglifyJsProcessor {
+
+    private UglifyJsRunner uglifyJsRunner;
+
+    public UglifyJsProcessor() {
+        this(new UglifyJsRunner());
+    }
+
+    public UglifyJsProcessor(UglifyJsRunner uglifyJsRunner) {
+        this.uglifyJsRunner = uglifyJsRunner;
+    }
+
+    private final class UglifyJsWebpipe extends ProcessingWebpipe {
+
+        private boolean uglify;
+
+        private String revervedNames;
+
+        private UglifyJsWebpipe(Webpipe webpipe, boolean uglify, String revervedNames) {
+            super(webpipe);
+            this.uglify = uglify;
+            this.revervedNames = revervedNames;
+        }
+
+        @Override
+        protected WebpipeOutput fetchContent() throws Exception {
+            return new WebpipeOutput(uglifyJsRunner.run(webpipe, uglify, revervedNames));
+        }
+    }
+
+    public Webpipe createProcessingWebpipe(Webpipe source, boolean uglify, String revervedNames) {
+        return new UglifyJsWebpipe(source, uglify, revervedNames);
+    }
+
+    public ProcessingWebpipeFactory createFactory(final boolean uglify, final String revervedNames) {
+        return new ProcessingWebpipeFactory() {
+            @Override
+            public Webpipe createProcessingWebpipe(Webpipe source) {
+                return new UglifyJsWebpipe(source, uglify, revervedNames);
+            }
+        };
+    }
+
+}
---
+++ webpipes-uglify2/src/java/org/hibnet/webpipes/processor/uglify/UglifyJsRunner.java
@@ -0,0 +1,57 @@
+/*
+ *  Copyright 2014-2015 WebPipes contributors
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.hibnet.webpipes.processor.uglify;
+
+import org.hibnet.webpipes.Webpipe;
+import org.hibnet.webpipes.processor.rhino.RhinoRunner;
+import org.hibnet.webpipes.resource.ClasspathResource;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.ScriptableObject;
+
+/**
+ * Compress js using uglifyJs utility.
+ */
+public class UglifyJsRunner extends RhinoRunner {
+
+    private ClasspathResource invokeResource;
+
+    private ClasspathResource defaultOptionsResource;
+
+    public UglifyJsRunner() {
+        invokeResource = new ClasspathResource("invoke.js", UglifyJsRunner.class);
+        defaultOptionsResource = new ClasspathResource("options.js", UglifyJsRunner.class);
+    }
+
+    @Override
+    protected void initScope(Context context, ScriptableObject globalScope) throws Exception {
+        evaluateFromClasspath(context, globalScope, "/org/hibnet/webpipes/processor/uglify/init.js");
+        evaluateFromClasspath(context, globalScope, "/org/hibnet/webpipes/processor/uglify/uglifyJs.min.js");
+    }
+
+    public String run(Webpipe webpipe, boolean uglify, String reservedNames) throws Exception {
+        String content = webpipe.getContent().getMain();
+        Context context = enterContext();
+        try {
+            Scriptable scope = createLocalScope(context);
+            String optionsAsJson = String.format(defaultOptionsResource.getContent().getMain(), !uglify, reservedNames);
+            String script = String.format(invokeResource.getContent().getMain(), toJSMultiLineString(content), optionsAsJson);
+            return evaluate(context, scope, script);
+        } finally {
+            Context.exit();
+        }
+    }
+}
